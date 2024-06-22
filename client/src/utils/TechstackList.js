dbeccdfc534cdd21bd744c7b9240618d3177c011
MAHESH2056 <maheschy2323@gmail.com>
2024-06-22 11:03:58 +0545

    Create Technologies Stack

---
+++ client/src/utils/TechstackList.js
@@ -0,0 +1,138 @@
+import { BsFiletypeHtml, BsFiletypeCss, BsBootstrap, BsFiletypeXml  } from "react-icons/bs";
+import { FaNodeJs } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiPython ,
+  SiJavascript,
+  SiMui,
+  SiReact,
+  SiTailwindcss,
+  SiFigma,
+  SiAdobephotoshop,
+  SiMongodb,
+  SiMysql,
+  SiTypescript,
+  SiGithub,
+  SiWordpress,
+  SiFirebase,
+  SiExpress,
+  SiAngular,
+} from "react-icons/si";
+import {FaJava } from "react-icons/fa";
+export const TechstackList = [
+  {
+    _id: 1,
+    name: "HTML",
+    icon: BsFiletypeHtml,
+  },
+  {
+    _id: 2,
+    name: "CSS",
+    icon: BsFiletypeCss,
+  },
+  {
+    _id: 3,
+    name: "Bootstrap",
+    icon: BsBootstrap,
+  },
+  {
+    _id: 4,
+    name: "XML",
+    icon: BsFiletypeXml,
+  },
+  {
+    _id: 5,
+    name: "Java",
+    icon: FaJava,
+  },
+  {
+    _id: 6,
+    name: "Python",
+    icon: SiPython,
+  },
+  {
+    _id: 7,
+    name: "Javascript",
+    icon: SiJavascript,
+  },
+  {
+    _id: 8,
+    name: "TypeScript",
+    icon: SiTypescript,
+  },
+  {
+    _id: 9,
+    name: "Angular",
+    icon: SiAngular,
+  },
+  {
+    _id: 10,
+    name: "React JS",
+    icon: SiReact,
+  },
+  {
+    _id: 11,
+    name: "Nextjs",
+    icon: TbBrandNextjs,
+  },
+  {
+    _id: 12,
+    name: "React MUI",
+    icon: SiMui,
+  },
+  {
+    _id: 13,
+    name: "Tailwind ",
+    icon: SiTailwindcss,
+  },
+  {
+    _id: 14,
+    name: "Figma ",
+    icon: SiFigma,
+  },
+  {
+    _id: 15,
+    name: "Photoshop ",
+    icon: SiAdobephotoshop,
+  },
+  {
+    _id: 16,
+    name: "React Native ",
+    icon: SiReact,
+  },
+  {
+    _id: 17,
+    name: "Node JS ",
+    icon: FaNodeJs,
+  },
+  {
+    _id: 18,
+    name: "Express JS ",
+    icon: SiExpress,
+  },
+  {
+    _id: 19,
+    name: "Wordpress ",
+    icon: SiWordpress,
+  },
+  {
+    _id: 20,
+    name: "Mongodb ",
+    icon: SiMongodb,
+  },
+  {
+    _id: 21,
+    name: "SQL ",
+    icon: SiMysql,
+  },
+  {
+    _id: 22,
+    name: "Firebase ",
+    icon: SiFirebase,
+  },
+  {
+    _id: 23,
+    name: "Git / Github ",
+    icon: SiGithub,
+  },
+];
