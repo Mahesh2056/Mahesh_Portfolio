88fe95dcf7865377d5151b4f642e18c73a0cc80b
MAHESH2056 <maheschy2323@gmail.com>
2024-06-22 05:49:12 +0545

    Create Home Section

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,38 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import Resume from "../../assets/docs/MAHESH CHAUDHARY.pdf";
 import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <>
+    <div className="container-fluid home-container">
+      <div className="container home-content">
+        <h1>Hi I'm a</h1>
+        <h2>
+          <Typewriter
+            options={{
+              strings: [
+                "Android Developer!", 
+                "Full Stack Developer!",
+                "Mern Stack Developer!",
+                "MySQL!",
+                "Networking!",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h2>
+        <div className="home-buttons">
+          <button className="btn btn-hire">Hire Me</button>
+          <a className="btn btn-cv" href={Resume} download="MAHESH CHAUDHARY.pdf">
+            My Resume
+          </a>
+        </div>
+      </div>
     </div>
+    </>
   );
 };
 
